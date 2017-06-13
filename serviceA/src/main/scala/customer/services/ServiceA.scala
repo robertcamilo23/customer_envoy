a19bc89fe9a88961ad054f43926a3c5b6ee20bdc
Andres Herrera <andresherrera@seven4n.com>
2017-06-13 09:20:56 -0500

    ADD service call

--- serviceA/src/main/scala/customer/services/ServiceA.scala
+++ serviceA/src/main/scala/customer/services/ServiceA.scala
@@ -2,6 +2,7 @@ package customer.services
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import customer.adapters.CIAdapter
 import customer.domain.Request
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
@@ -13,6 +14,8 @@ trait ServiceA {
         entity(as[Request]) {
           request: Request =>
             complete {
+              CIAdapter.callFirstService( request )
+              CIAdapter.callSecondService( request )
               s"got customer with name ${request.name}"
             }
         }
