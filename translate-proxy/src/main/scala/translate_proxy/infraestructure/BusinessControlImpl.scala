0eb31af92fb018e64c1a4707794db920d6a8a3c8
Robert Martinez <robertmartinez@seven4n.com>
2017-06-13 09:57:45 -0500

    cleaning code

--- translate-proxy/src/main/scala/translate_proxy/infraestructure/BusinessControlImpl.scala
+++ translate-proxy/src/main/scala/translate_proxy/infraestructure/BusinessControlImpl.scala
@@ -13,12 +13,6 @@ class BusinessControlImpl extends BusinessControl {
   private[this] val logger = Logger.getLogger(classOf[BusinessControlImpl].getName)
 
   override def validate(translateProxy: TranslateProxy): Future[Either[TranslateProxyError, TranslateProxyResponse]] = {
-
-    logger.info("***********************")
-    logger.info("... Consultando servicio cifrado")
-    logger.info("... Consultando servicio traduccion")
-    logger.info("***********************")
-
     val response = getValue(translateProxy)
 
     response map {
@@ -26,16 +20,17 @@ class BusinessControlImpl extends BusinessControl {
     } recover {
       case e: Exception => Left(TranslateProxyError(e.getMessage, 404))
     }
-
   }
 
   def getValue(translateProxy: TranslateProxy): Future[TranslateProxyResponse] = {
+    logger.info("***********************")
+    logger.info("... Consultando servicio cifrado")
+    logger.info("... Consultando servicio traduccion")
+    logger.info("***********************")
     val _token: String = "12345678Prueba$%&/"
-    //    Future.successful(
-    //      TranslateProxyResponse(translateProxy.name, translateProxy.age, _token)
-    //    )
-    Future {
-      throw new Exception("Hubo un problema el Hp !!!")
-    }
+    Future.successful(TranslateProxyResponse(translateProxy.name, translateProxy.age, _token))
+    Future.failed(new Exception("Hubo un problema el Hp !!!"))
+
+    //TODO Make petition to Envoy using http1 (envoy should convert that petition into gRPC)
   }
 }
